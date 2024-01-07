eff8d04a222e27f7a10821bd9e6476ca76714c3c
Rahul Dey <rahuldey832000@gmail.com>
2024-01-07 22:52:39 +0530

    Add version in api routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Testing start and run commands");
+import express from "express";
+import { configDotenv } from "dotenv";
+import v1Router from "./v1/routes/index.js";
+
+configDotenv();
+
+// create the express app.
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// add routers
+app.use("/api/v1", v1Router);
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`⚙️ server is listening on port 👉 ${PORT}`);
+});
