1d7cf3f5c4750ef0f9d273704ea6c3e83901c4f5
Rahul Dey <rahuldey832000@gmail.com>
2024-01-25 00:37:38 +0530

    Add Swagger Docs Setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { configDotenv } from "dotenv";
 import app from "./app.js";
+import v1SwaggerDocs from "../src/v1/swagger.js";
+
 configDotenv();
 
 const PORT = process.env.PORT || 3000;
@@ -7,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 // start the server
 const server = app.listen(PORT, () => {
   console.log(`⚙️ server is listening on port 👉 ${PORT}`);
+
+  // Add swagger documentation
+  v1SwaggerDocs(app, PORT);
 });
 
 export default server;
