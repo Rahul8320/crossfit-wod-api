fe4ed030f2f26ff1fc2cffdd89651f874260fbe0
Rahul Dey <rahuldey832000@gmail.com>
2024-01-27 19:10:59 +0530

    Add test case for get workout by id

--- test/index.test.js
+++ test/index.test.js
@@ -8,15 +8,91 @@ describe("crossfit's test cases", () => {
 });
 
 describe("Workout API", () => {
-  it("GET /workouts --> Returns a list of workouts", () => {});
+  it("GET /workouts --> Returns a list of workouts", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts?pageSize=5&pageNum=2")
+      .expect("Content-Type", /json/)
+      .expect(200);
 
-  it("GET /workouts/id --> Returns specific workout by ID", () => {});
+    expect(res.body.status).toBe(200);
+    expect(res.body.message).toBe("Workouts successfully fetched");
+    expect(res.body.data.pageNumber).toBe(2);
+    expect(res.body.data.pageSize).toBe(5);
 
-  it("GET /workouts/id --> Returns 404 if not found", () => {});
-
-  it("POST /workouts --> Returns newly created workout", () => {});
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        status: expect.any(Number),
+        message: expect.any(String),
+        data: expect.objectContaining({
+          pageNumber: expect.any(Number),
+          pageSize: expect.any(Number),
+          total: expect.any(Number),
+          workouts: expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(String),
+              name: expect.any(String),
+              mode: expect.any(String),
+              equipment: expect.any(Array),
+              exercises: expect.any(Array),
+              trainerTips: expect.any(Array),
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String),
+            }),
+          ]),
+        }),
+      })
+    );
+  });
 
   it("GET /workouts --> Validates request body", () => {});
+
+  it("GET /workouts/id --> Returns specific workout by ID", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts/61dbae02-c147-4e28-863c-db7bd402b2d6")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(res.body.status).toBe(200);
+    expect(res.body.message).toBe("Workout successfully fetched");
+
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        status: expect.any(Number),
+        message: expect.any(String),
+        data: expect.objectContaining({
+          id: expect.any(String),
+          name: expect.any(String),
+          mode: expect.any(String),
+          equipment: expect.any(Array),
+          exercises: expect.any(Array),
+          trainerTips: expect.any(Array),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        }),
+      })
+    );
+  });
+
+  it("GET /workouts/id --> Returns 404 if not found", () => {
+    return request(app)
+      .get("/api/v1/workouts/61dbae02-c147-4e28-863c-db7bd402b2d9")
+      .expect("Content-Type", /json/)
+      .expect(404);
+  });
+
+  it("GET /workouts/id --> Returns 400 for invalid ID", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts/invalid-id")
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors[0].msg).toBe(
+      "Invalid Workout ID. Please enter a valid workout ID"
+    );
+  });
+
+  it("POST /workouts --> Returns newly created workout", () => {});
 
   it("POST /workouts --> Validates request body", () => {});
 });
