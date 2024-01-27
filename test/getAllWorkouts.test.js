e9138f56cdd4f202472b6fd961aef6549fbbf7c4
Rahul Dey <rahuldey832000@gmail.com>
2024-01-27 19:44:55 +0530

    Add Test Case for get all workout route

--- test/getAllWorkouts.test.js
+++ test/getAllWorkouts.test.js
@@ -1,25 +1,135 @@
 import request from "supertest";
 import app from "../src/index.js";
 
-let server;
+describe("GET All Workouts Route", () => {
+  it("GET /workouts --> Returns a list of workouts", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts?pageSize=5&pageNum=2")
+      .expect("Content-Type", /json/)
+      .expect(200);
 
-beforeAll(() => {
-  server = app;
-});
+    expect(res.body.status).toBe(200);
+    expect(res.body.message).toBe("Workouts successfully fetched");
+    expect(res.body.data.pageNumber).toBe(2);
+    expect(res.body.data.pageSize).toBe(5);
 
-afterAll(async () => {
-  if (server) {
-    server.close();
-  }
-});
-
-describe("Get All Workouts Api", () => {
-  it("Gives success response with json data", async () => {
-    const response = await request(app).get(
-      "/api/v1/workouts?pageSize=5&pageNum=2"
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        status: expect.any(Number),
+        message: expect.any(String),
+        data: expect.objectContaining({
+          pageNumber: expect.any(Number),
+          pageSize: expect.any(Number),
+          total: expect.any(Number),
+          workouts: expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(String),
+              name: expect.any(String),
+              mode: expect.any(String),
+              equipment: expect.any(Array),
+              exercises: expect.any(Array),
+              trainerTips: expect.any(Array),
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String),
+            }),
+          ]),
+        }),
+      })
     );
+  });
 
-    expect(response.status).toBe(200);
-    // expect(response.body).toBe(JSON);
+  it("GET /workouts --> Returns 400 for no required params", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts")
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors.length).toBe(4);
+
+    expect(res.body.errors[0].path).toBe("pageSize");
+    expect(res.body.errors[0].msg).toBe("Page Size cannot be empty");
+
+    expect(res.body.errors[1].path).toBe("pageSize");
+    expect(res.body.errors[1].msg).toBe("Page Size must be a number!");
+
+    expect(res.body.errors[2].path).toBe("pageNum");
+    expect(res.body.errors[2].msg).toBe("Page Number cannot be empty");
+
+    expect(res.body.errors[3].path).toBe("pageNum");
+    expect(res.body.errors[3].msg).toBe("Page Number must be a number!");
+  });
+
+  it("GET /workouts --> Returns 400 for invalid required params", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts?pageSize=true&pageNum=false")
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors.length).toBe(2);
+
+    expect(res.body.errors[0].path).toBe("pageSize");
+    expect(res.body.errors[0].msg).toBe("Page Size must be a number!");
+
+    expect(res.body.errors[1].path).toBe("pageNum");
+    expect(res.body.errors[1].msg).toBe("Page Number must be a number!");
+  });
+
+  it("GET /workouts --> Returns 400 for invalid all params", async () => {
+    const res = await request(app)
+      .get(
+        "/api/v1/workouts?pageSize=true&pageNum=false&mode=12&equipment=9090"
+      )
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors.length).toBe(4);
+
+    expect(res.body.errors[0].path).toBe("mode");
+    expect(res.body.errors[0].msg).toBe("Mode cannot be a number!");
+
+    expect(res.body.errors[1].path).toBe("equipment");
+    expect(res.body.errors[1].msg).toBe("Equipment cannot be a number!");
+
+    expect(res.body.errors[2].path).toBe("pageSize");
+    expect(res.body.errors[2].msg).toBe("Page Size must be a number!");
+
+    expect(res.body.errors[3].path).toBe("pageNum");
+    expect(res.body.errors[3].msg).toBe("Page Number must be a number!");
+  });
+
+  it("GET /workouts --> Returns 400 for invalid pageSize param", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts?pageSize=true&pageNum=1")
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors.length).toBe(1);
+
+    expect(res.body.errors[0].path).toBe("pageSize");
+    expect(res.body.errors[0].msg).toBe("Page Size must be a number!");
+  });
+
+  it("GET /workouts --> Returns 400 for invalid pageNum param", async () => {
+    const res = await request(app)
+      .get("/api/v1/workouts?pageSize=3&pageNum=false")
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors.length).toBe(1);
+
+    expect(res.body.errors[0].path).toBe("pageNum");
+    expect(res.body.errors[0].msg).toBe("Page Number must be a number!");
+  });
+
+  it("GET /workouts --> Validates request body", () => {
+    return request(app)
+      .get("/api/v1/workouts?pageSize=true&pageNum=false&mode=12")
+      .expect("Content-Type", /json/)
+      .expect(400);
   });
 });
--- test/index.test.js
+++ test/index.test.js
@@ -8,44 +8,6 @@ describe("crossfit's test cases", () => {
 });
 
 describe("Workout API", () => {
-  it("GET /workouts --> Returns a list of workouts", async () => {
-    const res = await request(app)
-      .get("/api/v1/workouts?pageSize=5&pageNum=2")
-      .expect("Content-Type", /json/)
-      .expect(200);
-
-    expect(res.body.status).toBe(200);
-    expect(res.body.message).toBe("Workouts successfully fetched");
-    expect(res.body.data.pageNumber).toBe(2);
-    expect(res.body.data.pageSize).toBe(5);
-
-    expect(res.body).toEqual(
-      expect.objectContaining({
-        status: expect.any(Number),
-        message: expect.any(String),
-        data: expect.objectContaining({
-          pageNumber: expect.any(Number),
-          pageSize: expect.any(Number),
-          total: expect.any(Number),
-          workouts: expect.arrayContaining([
-            expect.objectContaining({
-              id: expect.any(String),
-              name: expect.any(String),
-              mode: expect.any(String),
-              equipment: expect.any(Array),
-              exercises: expect.any(Array),
-              trainerTips: expect.any(Array),
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
-            }),
-          ]),
-        }),
-      })
-    );
-  });
-
-  it("GET /workouts --> Validates request body", () => {});
-
   it("GET /workouts/id --> Returns specific workout by ID", async () => {
     const res = await request(app)
       .get("/api/v1/workouts/61dbae02-c147-4e28-863c-db7bd402b2d6")
